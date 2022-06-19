4ffbbea56df465b86b3473c8d6395b00a7054a2d
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-19 19:13:05 -0400

    Added operator

--- calculator.js
+++ calculator.js
@@ -15,3 +15,23 @@ function divide(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function operator(operation, firstNum, secondNum) {
+    let result;
+    if (operation == "+") {
+        result = add(firstNum, secondNum);
+        console.log(result)
+
+    } else if (operation == '-') {
+        result = subtract(firstNum, secondNum);
+        console.log(result)
+    } else if (operation == "/") {
+        result = divide(firstNum, secondNum)
+        console.log(result)
+    } else if (operation == "*") {
+        result = multiply(firstNum, secondNum)
+        console.log(result)
+    } else {
+        console.log("Something is wrong ")
+    }
+}
