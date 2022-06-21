c3e357e72761a9040c1f3aff3957133d02e97bc9
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-21 01:15:39 -0400

    Upload Function

--- calculator.js
+++ calculator.js
@@ -12,13 +12,30 @@ let operatorName = "";
 let isEqualClicked = false;
 
 numbers.forEach(number => {
+
     number.addEventListener("click", e => {
         if (operatorName == "") {
             firstNum += e.target.innerText;
+            displayValue = firstNum;
+            input.innerHTML = displayValue;
             console.log(firstNum)
         } else {
-            secondNum += e.target.innerText;
-            console.log(secondNum)
+            if (isEqualClicked) {
+                secondNum = "";
+                operatorName = "";
+                isEqualClicked = false;
+                result.innerHTML = "0";
+                firstNum = e.target.innerText;
+                displayValue = firstNum;
+                input.innerHTML = displayValue;
+
+            } else {
+                secondNum += e.target.innerText;
+                displayValue = firstNum + operatorName + secondNum;
+                input.innerHTML = displayValue;
+                console.log(secondNum)
+            }
+
         }
     })
 });
@@ -26,16 +43,29 @@ numbers.forEach(number => {
 operators.forEach(op => {
     op.addEventListener("click", e => {
         operatorName = e.target.innerText;
+        displayValue += operatorName;
+        input.innerHTML = displayValue;
         console.log(operatorName)
     })
 })
 
 equalBtn.addEventListener("click", e => {
     isEqualClicked = true;
-    operator(operatorName, firstNum, secondNum);
+    let calculatedResult = operator(operatorName, firstNum, secondNum);
+    result.innerHTML = calculatedResult;
 
 })
 
+clear.addEventListener("click", e => {
+
+    let displayValue = "";
+    firstNum = "";
+    secondNum = "";
+    operatorName = "";
+    // isEqualClicked = false;
+    result.innerHTML = "0";
+    input.innerHTML = "";
+})
 
 
 
@@ -64,18 +94,18 @@ function operator(operation, firstNum, secondNum) {
         console.log(resultAdd)
         return resultAdd;
     } else if (operation == '-') {
-        const resutlSub = subtract(firstNum, secondNum)
+        const resultSub = subtract(firstNum, secondNum)
         console.log(resultSub)
-        return resutlSub;
+        return resultSub;
     } else if (operation == "÷") {
         const resultDivide = divide(firstNum, secondNum)
         console.log(resultDivide)
         return resultDivide;
-    } else if (operation == "X") {
+    } else if (operation == "x") {
         const resultMultiply = multiply(firstNum, secondNum)
         console.log(resultMultiply)
         return resultMultiply;
     } else {
-        return "Something is wrong ";
+        return "Error";
     }
 }
